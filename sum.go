83fbe63dbe1d2171ddeb760e9010d311c21dc0fc
sindhu <sindhu.yallali318@gmail.com>
2025-05-19 20:18:37 +0530

    addedmultiplication

--- sum.go
+++ sum.go
@@ -6,7 +6,10 @@ func Sum(a int, b int) int {
 	return a + b
 }
 func main() {
+	d := multiply(20, 39)
 
 	c := Sum(10, 20)
 	fmt.Println(c)
+	fmt.Println(d)
+
 }
--- sum_test.go
+++ sum_test.go
@@ -12,5 +12,9 @@ func TestMain(t *testing.T) {
 		t.Errorf("Expected 30, but got %d", c)
 	}
 	fmt.Println("sindhu")
+	d := multiply(20, 30)
+	if d != 600 {
+		t.Errorf("Expected 30, but got %d", d)
+	}
 
 }
