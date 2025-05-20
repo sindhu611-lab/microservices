6060f887927f507b8a98a8e97ac7cf1786d844ec
sindhu <sindhu.yallali318@gmail.com>
2025-05-20 19:53:09 +0530

    dividetwonumbers

--- sum_test.go
+++ sum_test.go
@@ -14,7 +14,11 @@ func TestMain(t *testing.T) {
 	fmt.Println("sindhu")
 	d := multiply(20, 30)
 	if d != 600 {
-		t.Errorf("Expected 30, but got %d", d)
+		t.Errorf("Expected 600, but got %d", d)
+		e := divide(10, 3)
+		if e != 3 {
+			t.Errorf("Expected 3,but got %d", e)
+		}
 	}
 
 }
